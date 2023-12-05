f8054cb03440daadac32920113d426113cadedc4
unxpctdchrs <theunexpectedchris@gmail.com>
2023-12-05 20:57:24 +0700

    Initial commit

---
+++ app/src/main/java/com/ucokers/teamseeker/activity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.ucokers.teamseeker.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ucokers.teamseeker.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
